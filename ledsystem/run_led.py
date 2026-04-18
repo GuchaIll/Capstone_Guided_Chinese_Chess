68dced7ef885313b3829fb5e51e95354e00664b1
claireglee <clairele@andrew.cmu.edu>
2026-04-18 12:30:20 -0400

    starting + ending sequences

--- ledsystem/run_led.py
+++ ledsystem/run_led.py
@@ -1,11 +1,23 @@
+# from led_board import LEDBoard
+
+# led = LEDBoard()
+
+# led.set_fen("rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR")
+
+# led.show_moves("soldier", 6, 0)
+
+# # simulate opponent move
+# # led.show_opponent_move(from_r, from_c, to_r, to_c)
+# led.show_opponent_move(2, 4, 4, 4)
+
 from led_board import LEDBoard
 
 led = LEDBoard()
 
-led.set_fen("rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR")
+# Show zones
+led.show_start_zones()
 
-led.show_moves("soldier", 6, 0)
+input("Press Enter to celebrate red win...")
 
-# simulate opponent move
-# led.show_opponent_move(from_r, from_c, to_r, to_c)
-led.show_opponent_move(2, 4, 4, 4)
+# Celebrate red side win
+led.celebrate_win("red")
