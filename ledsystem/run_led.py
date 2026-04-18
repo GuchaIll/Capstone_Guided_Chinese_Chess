ae0227e254fa311f9644fdf93a13cde4f709a5da
claireglee <clairele@andrew.cmu.edu>
2026-04-18 12:09:48 -0400

    Create run_led.py

---
+++ ledsystem/run_led.py
@@ -0,0 +1,11 @@
+from led_board import LEDBoard
+
+led = LEDBoard()
+
+led.set_fen("rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR")
+
+led.show_moves("soldier", 6, 0)
+
+# simulate opponent move
+# led.show_opponent_move(from_r, from_c, to_r, to_c)
+led.show_opponent_move(2, 4, 4, 4)
